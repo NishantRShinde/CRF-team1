98981fbfdc1a3797e6a9810838a5891d88573107
Banda <Madan.Banda@nielseniq.com>
2023-04-12 12:39:11 +0530

    removed untitled in master

--- Project_demo/src/app/app.module.ts
+++ Project_demo/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UntitledReportComponent } from './untitled-report/untitled-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UntitledReportComponent
   ],
   imports: [
     BrowserModule,
