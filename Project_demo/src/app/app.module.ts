e22767736f0421f6711e283281b1004f595c4371
Banda <Madan.Banda@nielseniq.com>
2023-04-12 12:11:28 +0530

    added untitledreport component

--- Project_demo/src/app/app.module.ts
+++ Project_demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UntitledReportComponent } from './untitled-report/untitled-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UntitledReportComponent
   ],
   imports: [
     BrowserModule,
---
+++ Project_demo/src/app/untitled-report/untitled-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-untitled-report',
+  templateUrl: './untitled-report.component.html',
+  styleUrls: ['./untitled-report.component.css']
+})
+export class UntitledReportComponent {
+
+}
