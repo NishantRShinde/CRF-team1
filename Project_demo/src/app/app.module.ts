7f39490e9907149780f8d189d603e8d437c38af5
Nishant R Shinde <Nishant.Shinde@nielseniq.com>
2023-04-13 16:15:21 +0530

    Brandbar Done

--- Project_demo/src/app/app.module.ts
+++ Project_demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandbarComponent } from './brandbar/brandbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ Project_demo/src/app/brandbar/brandbar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brandbar',
+  templateUrl: './brandbar.component.html',
+  styleUrls: ['./brandbar.component.css'],
+})
+export class BrandbarComponent {
+  previous: any;
+  isFirst: boolean = true;
+
+  highlight(name: string): void {
+    if (this.isFirst) {
+      this.isFirst = false;
+      let item = document.querySelector(`.${name}`);
+      item?.classList.add('border-class');
+      this.previous = item;
+    } else {
+      this.previous.classList.remove('border-class');
+      let item = document.querySelector(`.${name}`);
+      item?.classList.add('border-class');
+      this.previous = item;
+    }
+  }
+}
