76997a76e14a1831fb088d7fa4eeb82c06c05d20
Pratik Paulkar <pratik.paulkar@nielseniq.com>
2023-04-17 15:08:40 +0530

    Created seperate sidebars

--- Project_demo/src/app/data-summary/data-summary.component.ts
+++ Project_demo/src/app/data-summary/data-summary.component.ts
@@ -6,29 +6,136 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-summary.component.css']
 })
 export class DataSummaryComponent {
-//  myFunction(): any{
-//   alert("Successfully executed!")
-//  }
-//  openNav(): any {
-//   x =  document.getElementById("mySidebar")
-//   y =  document.getElementById("main")
-//   if(X!=null && y!=null){
-//     x.style.width = "330px";
-//     y.style.marginRight = "330px";
-//   }
-//   else{
-//     alert("oops! Invalid Entry.")
-//   }
-// }
+  //  myFunction(): any{
+  //   alert("Successfully executed!")
+  //  }
+  previous: any;
+  isFirst: boolean = true;
+  openClose(sidebar: String) {
+    if (this.isFirst) {
 
-openNav(): any {
-  document.getElementById("mySidebar")!.style.width = "325px";
-  document.getElementById("main")!.style.marginRight = "325px";
-}
+      this.openNav(sidebar);
+      this.isFirst = false;
+    }
+    else {
+      this.closeNav();
+      this.openNav(sidebar);
+    }
+  }
 
-closeNav() {
-  document.getElementById("mySidebar")!.style.width = "0";
-  document.getElementById("main")!.style.marginRight= "0";
-}
+  openNav(sidebar: String) {
+    var z = document.getElementById("main");
+    switch (sidebar) {
+      case "facts":
+        var x = document.getElementById("mySidebar")
+        var y = document.getElementById("factsSidebar")
+        if (x != null && y != null && z != null) {
+          x.style.width = "325px";
+          y.style.width = "325px";
+          z.style.marginRight = "325px";
+        }
+        else {
+          alert("oops! Invalid Entry.")
+        }
+        this.previous = y
+        break;
+
+      case "products":
+        var x = document.getElementById("mySidebar")
+        var y = document.getElementById("productsSidebar")
+        if (x != null && y != null && z != null) {
+          x.style.width = "325px";
+          y.style.width = "325px";
+          z.style.marginRight = "325px";
+        }
+        else {
+          alert("oops! Invalid Entry.");
+        }
+        this.previous = y
+        break;
+        
+    }
+  }
+
+  closeNav(){
+    var x = document.getElementById("mySidebar")
+    var z = document.getElementById("main");
+    if (x != null && this.previous != null && z != null){
+      x.style.width = '0px';
+      this.previous.style.width = "0px";
+      z.style.marginRight = "0px";
+    }
+    else{
+      alert("oops! Invalid Entry.");
+    }
+  }
+
+  // openFactsNav() {
+  //   var x = document.getElementById("mySidebar")
+  //   var y = document.getElementById("factsSidebar")
+  //   var z = document.getElementById("main")
+  //   if (x != null && y != null && z != null) {
+  //     x.style.width = "325px";
+  //     y.style.width = "325px";
+  //     z.style.marginRight = "325px";
+  //   }
+  //   else {
+  //     alert("oops! Invalid Entry.")
+  //   }
+  // }
+
+  // closeFactsNav() {
+  //   var x = document.getElementById("mySidebar")
+  //   var y = document.getElementById("factsSidebar")
+  //   var z = document.getElementById("main")
+  //   if (x != null && y != null && z != null) {
+  //     x.style.width = "0px";
+  //     y.style.width = "0px";
+  //     z.style.marginRight = "0px";
+  //   }
+  //   else {
+  //     alert("oops! Invalid Entry.")
+  //   }
+  // }
+
+  // openProductsNav() {
+  //   var x = document.getElementById("mySidebar")
+  //   var y = document.getElementById("productsSidebar")
+  //   var z = document.getElementById("main")
+  //   if (x != null && y != null && z != null) {
+  //     x.style.width = "325px";
+  //     y.style.width = "325px";
+  //     z.style.marginRight = "325px";
+  //   }
+  //   else {
+  //     alert("oops! Invalid Entry.")
+  //   }
+  // }
+
+  // closeProductsNav() {
+  //   var x = document.getElementById("mySidebar")
+  //   var y = document.getElementById("productsSidebar")
+  //   var z = document.getElementById("main")
+  //   if (x != null && y != null && z != null) {
+  //     x.style.width = "0px";
+  //     y.style.width = "0px";
+  //     z.style.marginRight = "0px";
+  //   }
+  //   else {
+  //     alert("oops! Invalid Entry.")
+  //   }
+  // }
+
+  // openNav(): any {
+  //   document.getElementById("mySidebar")!.style.width = "325px";
+  //   document.getElementById("main")!.style.marginRight = "325px";
+  // }
+
+  // closeNav() {
+  //   document.getElementById("mySidebar")!.style.width = "0";
+  //   document.getElementById("main")!.style.marginRight= "0";
+  // }
+
+
 
 }
