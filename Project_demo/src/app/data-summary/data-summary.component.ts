07db4707ceb8e42cac88e212b30083cbe8a68a21
Pratik Paulkar <pratik.paulkar@nielseniq.com>
2023-04-17 11:57:05 +0530

    commit2

--- Project_demo/src/app/data-summary/data-summary.component.ts
+++ Project_demo/src/app/data-summary/data-summary.component.ts
@@ -9,9 +9,21 @@ export class DataSummaryComponent {
 //  myFunction(): any{
 //   alert("Successfully executed!")
 //  }
- openNav(): any {
-  document.getElementById("mySidebar")!.style.width = "250px";
-  document.getElementById("main")!.style.marginRight = "250px";
+//  openNav(): any {
+//   x =  document.getElementById("mySidebar")
+//   y =  document.getElementById("main")
+//   if(X!=null && y!=null){
+//     x.style.width = "330px";
+//     y.style.marginRight = "330px";
+//   }
+//   else{
+//     alert("oops! Invalid Entry.")
+//   }
+// }
+
+openNav(): any {
+  document.getElementById("mySidebar")!.style.width = "325px";
+  document.getElementById("main")!.style.marginRight = "325px";
 }
 
 closeNav() {
