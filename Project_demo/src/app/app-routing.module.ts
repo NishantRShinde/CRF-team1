eaf6ae4afc39cc2b12c418393aa09da7c1e5911a
shubham <19510453.dypit@dypvp.edu.in>
2023-04-18 17:30:27 +0530

    removed comments

--- Project_demo/src/app/app-routing.module.ts
+++ Project_demo/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DataSelectorComponent } from './data-selector/data-selector.component';
-import { InternalDataSelectorComponent } from './internal-data-selector/internal-data-selector.component';
-import { LastDataSelectorComponent } from './last-data-selector/last-data-selector.component';
+
 
 // const routes: Routes = [
 //   { path: '', component: AppComponent },
--- Project_demo/src/app/app.component.ts
+++ Project_demo/src/app/app.component.ts
@@ -12,23 +12,8 @@ export class AppComponent {
   title = 'Project_demo';
   buttonVisible = true;
 
-  // constructor(private dialog: MatDialog,
-  //   // public router: Router
-  //   ) { }
 
 
   openNewDialog(): void {
-    // this.router.navigate(['/second']);
-
-    //  this.buttonVisible = false;
-    // const dialogRef = this.dialog.open(DataSelectorComponent, {
-    //   width: '540px',
-    //   position: { top: '4.7%', left: '34%' },
-    //   panelClass: 'custom-dialog-container'
-    // });
-
-    // dialogRef.afterClosed().subscribe((result: any) => {
-    //   console.log(`Dialog result: ${result}`);
-    // });
   }
 }
