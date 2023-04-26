1d340addd1fd80f71f906f4f7ec251f2964d8cb2
Nishant R Shinde <Nishant.Shinde@nielseniq.com>
2023-04-26 10:32:14 +0530

    permissions sorted

--- Project_demo/src/app/brandbar/brandbar.component.ts
+++ Project_demo/src/app/brandbar/brandbar.component.ts
@@ -10,9 +10,11 @@ export class BrandbarComponent {
   constructor(
     public shimmerService: ShimmerEffectService,
     public datasetSelectorService: DatasetSelectorService
-  ) {}
+  ) {
+    console.log(typeof this.user.permissions[0].name);
+  }
 
-  currentlySelected: AllPermissions = AllPermissions.buildATable;
+  currentlySelected: string = AllPermissions.buildATable;
 
   //user object
   user: {
@@ -21,15 +23,15 @@ export class BrandbarComponent {
   } = {
     uname: 'Nishant.Shinde',
     permissions: [
-      { name: 'Monitor my business', route: '/monitorMyBusiness' },
-      { name: 'Choose a template', route: 'chooseATemplate' },
-      { name: 'Build a table', route: '/buildATable' },
-      { name: 'Find my stuff', route: '/findMyStuff' },
+      { name: AllPermissions.monitorMyBusiness, route: '/monitorMyBusiness' },
+      { name: AllPermissions.chooseATemplate, route: 'chooseATemplate' },
+      { name: AllPermissions.buildATable, route: '/buildATable' },
+      { name: AllPermissions.findMyStuff, route: '/findMyStuff' },
     ],
   };
 
-  changeCurrentlySelected(index: number): void {
-    this.currentlySelected = index;
+  changeCurrentlySelected(permission: string): void {
+    this.currentlySelected = permission;
     if (this.currentlySelected === AllPermissions.buildATable) {
       this.datasetSelectorService.isOnBuildATable = true;
     } else {
@@ -39,8 +41,8 @@ export class BrandbarComponent {
 }
 
 enum AllPermissions {
-  monitorMyBusiness,
-  chooseATemplate,
-  buildATable,
-  findMyStuff,
+  monitorMyBusiness = 'Monitor my business',
+  chooseATemplate = 'Choose a template',
+  buildATable = 'Build a table',
+  findMyStuff = 'Find my stuff',
 }
